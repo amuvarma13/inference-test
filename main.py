a2389bcd9045f5bdebd2a91f8a298d13b7ecffb9
Amartya Varma <amartya.varma@gmail.com>
2024-10-10 18:08:06 -0700

    f

---
+++ main.py
@@ -0,0 +1,36 @@
+import torch
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, Trainer, TrainingArguments, AutoTokenizer
+
+tokeniser_name = "meta-llama/Llama-3.2-3B-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(tokeniser_name)
+
+prompt = '''I'm not sure how we can prevent churn and grow revenue?'''
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+start_token = torch.tensor([[ 128259]], dtype=torch.int64)
+end_tokens = torch.tensor([[128009, 128260, 128261]], dtype=torch.int64)
+
+modified_input_ids = torch.cat([start_token, input_ids, end_tokens], dim=1)
+
+input_ids = modified_input_ids
+attention_mask = torch.ones_like(input_ids)
+
+input_ids = input_ids.to("cuda")
+attention_mask = attention_mask.to("cuda")
+stop_token = 128258
+
+model_name = "amuvarma/convo-fpsft-13k"
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
+
+generated_ids = model.generate(
+    input_ids=input_ids,
+    attention_mask=attention_mask,
+    max_length=3000,
+    num_return_sequences=1,
+    do_sample=True,
+    temperature=0.01,
+    eos_token_id=stop_token,
+)
+
+print(generated_ids.shape)
