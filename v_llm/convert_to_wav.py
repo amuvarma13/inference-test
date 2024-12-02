f1ee1b39e5471d224474a4923c259490205464c4
Amartya Varma <amartya.varma@gmail.com>
2024-12-01 16:20:11 -0800

    f

--- v_llm/convert_to_wav.py
+++ v_llm/convert_to_wav.py
@@ -1,4 +1,4 @@
-from v_llm.naturalspeech3_facodec.ns3_codec import FACodecEncoder, FACodecDecoder
+from naturalspeech3_facodec.ns3_codec import FACodecEncoder, FACodecDecoder
 from huggingface_hub import hf_hub_download
 import torch
 import librosa
