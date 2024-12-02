4c810a045e9869ddcf7ce7466c795b760bba5875
Amartya Varma <amartya.varma@gmail.com>
2024-12-01 16:30:54 -0800

    f

--- v_llm/app.py
+++ v_llm/app.py
@@ -4,7 +4,7 @@ from vllm import LLM, SamplingParams
 from typing import List
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
-from convert_to_wav import convert_to_wav
+from convert_to_wav import process_input_ids
 from extract_parts import extract_content_tokens
 # Initialize FastAPI app
 app = FastAPI()
@@ -35,7 +35,7 @@ async def generate_text(prompt: str):
         outputs = llm.generate([prompt], sampling_params)
         token_ids = outputs[0].outputs[0].token_ids
         # content_tokens_response = extract_content_tokens(token_ids)
-        wavs = convert_to_wav(token_ids)
+        wavs = process_input_ids(token_ids)
         return InferenceResponse(wavs=wavs.tolist())
     
     except Exception as e:
