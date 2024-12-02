7c9bdf4d3c0c8f9459a46ad8825732c986b32f90
Amartya Varma <amartya.varma@gmail.com>
2024-12-01 16:36:22 -0800

    f

--- v_llm/convert_to_wav.py
+++ v_llm/convert_to_wav.py
@@ -63,6 +63,7 @@ spk_embs = process_audio_and_get_vq_id()
 def process_input_ids(generated_ids):
 
     print("generated_ids", type(generated_ids))
+    generated_ids = torch.LongTensor(generated_ids).unsqueeze(0)
 
     token_to_find = 128257
     token_to_remove = 128263
